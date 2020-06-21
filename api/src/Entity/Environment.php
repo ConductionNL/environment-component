b2b094dcd7f677dfdfd09dd780f6a137e5e6992c
Ruben van der Linde <ruben@conduction.nl>
2020-06-21 22:07:02 +0200

    fix

--- api/src/Entity/Environment.php
+++ api/src/Entity/Environment.php
@@ -120,7 +120,6 @@ class Environment
      * @var string Should components be deployed to this environment with debuggin on or off?
      *
      * @example 1
-     * @default 1
      *
      * @Gedmo\Versioned
      * @Groups({"read", "write"})
