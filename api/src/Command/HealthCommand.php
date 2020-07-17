6ac7a483a4265929a4c0e9d8a1cf03fd6b6bb8b5
Ruben van der Linde <ruben@conduction.nl>
2020-07-17 12:09:28 +0200

    Rework on health command

--- api/src/Command/HealthCommand.php
+++ api/src/Command/HealthCommand.php
@@ -38,9 +38,9 @@ class HealthCommand extends Command
 
         // the full command description shown when running the command with
         // the "--help" option
-        ->setHelp('This command will perform a health check on all or a single cluster')
+        ->setHelp('This command will perform a health check on all or a single installation')
         ->setDescription('Perform health check on cluster')
-        ->addOption('cluster', null, InputOption::VALUE_OPTIONAL, 'the cluster that you want to health check');
+        ->addOption('installation', null, InputOption::VALUE_OPTIONAL, 'the installation that you want to health check');
     }
 
     /**
@@ -51,27 +51,21 @@ class HealthCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         /** @var string $version */
-        $componentId = $input->getOption('cluster');
+        $installationId = $input->getOption('installation');
 
-        if ($componentId) {
+        if ($installationId) {
             $clusters = $this->em->getRepository('App\Entity\Cluster')->findBy();
         } else {
-            $clusters = $this->em->getRepository('App\Entity\Cluster')->findAll();
+            $installations = $this->em->getRepository('App\Entity\Installation')->findAll();
         }
 
-        if (!$clusters || count($clusters) < 1) {
-            throw new InvalidOptionException(sprintf('No installable clusters could be found'));
+        if (!$installations || count($installations) < 1) {
+            throw new InvalidOptionException(sprintf('No installations could be found'));
         }
 
         $io->title('Starting health checks');
 
-        $installations = [];
 
-        foreach ($clusters as $cluster) {
-            $installations = array_merge($installations, $cluster->getInstallations()->toArray());
-        }
-
-        $io->text('Found '.count($clusters).' clusters to check');
         $io->text('Found '.count($installations).' installations to check');
 
         if (count($installations) > 0) {
@@ -79,17 +73,57 @@ class HealthCommand extends Command
         }
 
         $results = [];
+        $clusters =[];
+        $environment =[];
+
         foreach ($installations as $installation) {
             $health = $this->healthService->check($installation);
             $results[] = [$health->getDomain()->getName(), $health->getInstallation()->getEnvironment()->getName(), $health->getInstallation()->getName(), $health->getEndpoint(), $health->getStatus()];
 
+
+            // Lets create some statistical data
+            if(!array_key_exists( $health->getInstallation()->getEnvironment()->getCluster(), $clusters)){
+                $clusters[$health->getInstallation()->getEnvironment()->getCluster()] = ['health' => 0, 'installations' => 0 ];
+            }
+
+            if(!array_key_exists( $health->getInstallation()->getEnvironment(), $environment)){
+                $environment[$health->getInstallation()->getEnvironment()] = ['health' => 0, 'installations' => 0 ];
+            }
+
+            if($health->getStatus() == "OK"){
+                $clusters[$health->getInstallation()->getEnvironment()->getCluster()]['health'] ++;
+                $environment[$health->getInstallation()->getEnvironment()]['health']  ++;
+            }
+
+            $clusters[$health->getInstallation()->getEnvironment()->getCluster()]['installations'] ++;
+            $environment[$health->getInstallation()->getEnvironment()]['installations']  ++;
+
             $io->progressAdvance();
         }
+
+        // Let registr the statistical results to there proper entities
+        foreach ($clusters as $key => $value){
+
+            $key->setHealth($value['health']);
+            $key->setInstallations($value['installations']);
+
+            $this->em->persist($key);
+        }
+
+
+        foreach ($clusters as $key => $value){
+
+            $key->setHealth($value['health']);
+
+            $this->em->persist($key);
+        }
+
+        $this->em->flush();
 
         $io->text('All done');
 
         $io->table(
-            ['Cluster', 'Enviroment', 'Installation', 'Endpoint', 'Status'],
+            ['Domain', 'Enviroment', 'Installation', 'Endpoint', 'Status'],
             $results
         );
     }
